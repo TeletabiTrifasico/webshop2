649586f1f2bda8b785af2cb98b985a9b40323bf2
TeletabiTrifasico <hugojiba@gmail.com>
2025-03-15 19:42:56 +0100

    Fixed footer

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
 </head>
-<body data-user-logged-in="<?= isset($_SESSION['user_id']) ? 'true' : 'false' ?>">
+<body>
     <div class="toast-container position-fixed top-0 end-0 p-3"></div>
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
