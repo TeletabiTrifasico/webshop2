9d7fbf27552ccd56f763b396653a58576d0e5d5c
TeletabiTrifasico <hugojiba@gmail.com>
2025-03-31 13:22:33 +0200

    header logout + user buttons

--- Resources/js/components/layouts/AppHeader.vue
+++ Resources/js/components/layouts/AppHeader.vue
@@ -17,12 +17,20 @@
           </li>
         </ul>
 
-        <ul class="navbar-nav">
+        <ul class="navbar-nav align-items-center">
           <template v-if="isAuthenticated">
+            <li class="nav-item me-3">
+              <span class="nav-link text-light">
+                <i class="fas fa-user me-1"></i>
+                {{ user.username }}
+              </span>
+            </li>
             <li class="nav-item">
-              <a @click.prevent="handleLogout" 
-                 href="#" 
-                 class="nav-link">Logout</a>
+              <button @click="handleLogout" 
+                      class="btn btn-outline-danger">
+                <i class="fas fa-sign-out-alt me-1"></i>
+                Logout
+              </button>
             </li>
           </template>
           <template v-else>
@@ -61,3 +69,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.btn-outline-danger {
+  border-color: #dc3545;
+  color: #dc3545;
+  transition: all 0.3s ease;
+}
+
+.btn-outline-danger:hover {
+  background-color: #dc3545;
+  color: white;
+  transform: translateY(-2px);
+  box-shadow: 0 2px 4px rgba(220, 53, 69, 0.2);
+}
+
+.nav-link {
+  transition: color 0.3s ease;
+}
+
+.nav-link:hover {
+  color: #fff;
+}
+</style>
